6e1594e659b0638187aca0e3c97c87440cdf1d22
Daniel Quah <danielquah148@yahoo.com>
2017-05-09 21:53:40 +0800

    Added back test suit

---
+++ quiet/src/androidTest/java/org/quietmodem/Quiet/SilentTestSuite.java
@@ -0,0 +1,8 @@
+package org.quietmodem.Quiet;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({FrameTest.class})
+public class SilentTestSuite {}
